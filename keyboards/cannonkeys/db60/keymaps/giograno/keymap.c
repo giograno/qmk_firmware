e6550cdd2ed9e81e71a728f4ec1c35e219e33a98
Giovanni Grano <me@giograno.com>
2023-10-10 19:04:25 -0400

    fix command-alt order

--- keyboards/cannonkeys/db60/keymaps/giograno/keymap.c
+++ keyboards/cannonkeys/db60/keymaps/giograno/keymap.c
@@ -33,14 +33,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS,
     HYPR_T(KC_ESC), KC_A,    LCTL_T(KC_S),    LALT_T(KC_D),    LGUI_T(KC_F),    KC_G,    KC_H,                 RGUI_T(KC_J),    RALT_T(KC_K),    RCTL_T(KC_L),    KC_SCLN, KC_QUOT,           KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,                 KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    RSFT_T(KC_UP),
-    KC_LCTL, KC_LGUI, KC_LALT,                            LT(1,KC_SPC),                                          KC_RALT, RGUI_T(KC_LEFT), LT(2, KC_DOWN), RCTL_T(KC_RIGHT)
+    KC_LCTL, KC_LALT, KC_LGUI,                            LT(1,KC_SPC),                                          KC_RALT, RGUI_T(KC_LEFT), LT(2, KC_DOWN), RCTL_T(KC_RIGHT)
   ),
 
   [_FN_SYMBOLS] = LAYOUT_60_ansi(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______, _______, _______,
     _______, KC_PERC, KC_DLR, KC_LPRN, KC_RPRN, KC_GRAVE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,
-    _______, KC_HASH,  KC_CIRCUMFLEX, KC_LBRACKET, KC_RBRACKET, KC_TILD, KC_UNDS, KC_MINUS,  KC_EQUAL,  KC_PERCENT, _______,                   _______,
+    _______, KC_HASH,  KC_CIRCUMFLEX, KC_LBRC, KC_RBRC, KC_TILD, KC_UNDS, KC_MINUS,  KC_EQUAL,  KC_PERCENT, _______,                   _______,
     _______,  _______, _______,                            _______,                            _______, _______, _______, QK_BOOT
   ),
 
