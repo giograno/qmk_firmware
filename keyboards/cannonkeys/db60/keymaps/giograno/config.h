6bcf8fcd045c78603b0ebcb537e81e434a6a129b
Giovanni Grano <me@giograno.com>
2023-10-13 08:52:52 -0400

    better configs

--- keyboards/cannonkeys/db60/keymaps/giograno/config.h
+++ keyboards/cannonkeys/db60/keymaps/giograno/config.h
@@ -1,7 +1,7 @@
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 180
+#define TAPPING_TERM 200
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM 50
+#define QUICK_TAP_TERM 0
 
 #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
