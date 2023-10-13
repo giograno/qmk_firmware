4f3c024d9c74e666122363a9333120c61108b4a8
Giovanni Grano <me@giograno.com>
2023-10-13 08:38:20 -0400

    some order in the keymap

--- keyboards/cannonkeys/db60/keymaps/giograno/keymap.c
+++ keyboards/cannonkeys/db60/keymaps/giograno/keymap.c
@@ -16,6 +16,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
+// left hand home row mods
+#define HOME_S LCTL_T(KC_S)
+#define HOME_D LALT_T(KC_D)
+#define HOME_F LCTL_T(KC_F)
+
+// right hand home row mods
+#define HOME_J RGUI_T(KC_J)
+#define HOME_K RALT_T(KC_K)
+#define HOME_L RCTL_T(KC_L)
+
+// hyper and esc on caps lock
+#define ESC_HYP HYPR_T(KC_ESC)
+
+// arrows on tap, layer on hold
+#define LFT_ARR RALT_T(KC_LEFT)
+#define DWN_ARR LT(2, KC_DOWN)
+#define RGT_ARR RCTL_T(KC_RIGHT)
+#define UP_ARR  RSFT_T(KC_UP)
+
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -29,19 +48,19 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_60_ansi(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                 KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS,
-    HYPR_T(KC_ESC), KC_A,    LCTL_T(KC_S),    LALT_T(KC_D),    LGUI_T(KC_F),    KC_G,    KC_H,                 RGUI_T(KC_J),    RALT_T(KC_K),    RCTL_T(KC_L),    KC_SCLN, KC_QUOT,           KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,                 KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    RSFT_T(KC_UP),
-    KC_LCTL, KC_LALT, KC_LGUI,                            LT(1,KC_SPC),                                          KC_RALT, RGUI_T(KC_LEFT), LT(2, KC_DOWN), RCTL_T(KC_RIGHT)
+    ESC_HYP, KC_A,    HOME_S,  HOME_D,  HOME_F,  KC_G,    KC_H,   HOME_J,  HOME_K,  HOME_L,  KC_SCLN, KC_QUOT,           KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    UP_ARR,
+    KC_LCTL, KC_LALT, KC_LGUI,                            LT(1,KC_SPC),                      KC_RGUI, LFT_ARR, DWN_ARR,  RGT_ARR
   ),
 
   [_FN_SYMBOLS] = LAYOUT_60_ansi(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-    _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______, _______, _______,
-    _______, KC_PERC, KC_DLR, KC_LPRN, KC_RPRN, KC_GRAVE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,
-    _______, KC_HASH,  KC_CIRCUMFLEX, KC_LBRC, KC_RBRC, KC_TILD, KC_UNDS, KC_MINUS,  KC_EQUAL,  KC_PERCENT, _______,                   _______,
-    _______,  _______, _______,                            _______,                            _______, _______, _______, QK_BOOT
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,     KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+    _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_HOME, KC_PGDN,   KC_PGUP,  KC_END,  _______, _______, _______, _______,
+    _______, KC_PERC, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRAVE,KC_LEFT, KC_DOWN,   KC_UP,    KC_RGHT, _______, _______,          _______,
+    _______, KC_HASH, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_UNDS, KC_MINUS,  KC_EQUAL, KC_PERC, _______,                   _______,
+    _______, _______, _______,                            _______,                               _______, _______, _______, QK_BOOT
   ),
 
   [_MEDIA] = LAYOUT_60_ansi(
